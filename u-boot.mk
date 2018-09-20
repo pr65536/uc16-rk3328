ad61d24541e321032e99f14c577c0488da5b54fe
Devang Panchal <devang_panchal90@yahoo.com>
2018-09-20 19:08:28 +0530

    rk3328: Added rk33 firmware
    Add rk33 firmware as rkbin has removed tested firmware.
    
    Signed-off-by: Devang Panchal <devang_panchal90@yahoo.com>

--- u-boot.mk
+++ u-boot.mk
@@ -17,9 +17,9 @@ $(UBOOT_BIN): $(UBOOT_SRC)
 	$(MAKE) ARCH=$(ARCH) CROSS_COMPILE=${CC} -C $(UBOOT_SRC) $(UBOOT_DEFCONFIG)
 	$(MAKE) $(UBOOT_DEFCONFIG) -C $(UBOOT_SRC) -j$(CPUS) all
 	cd $(UBOOT_OUT) && ${TOOLPATH}/loaderimage --pack --uboot ./u-boot-dtb.bin uboot.img 0x200000
-	cd $(UBOOT_OUT) && ./tools/mkimage -n rk3328 -T rksd -d ../rkbin/rk33/rk3328_ddr_786MHz_v1.06.bin idbloader.img
-	cd $(UBOOT_OUT) && cat ../rkbin/rk33/rk3328_miniloader_v2.43.bin >> idbloader.img
-	cd $(UBOOT_OUT) && cp ../rkbin/rk33/rk3328_loader_ddr786_v1.06.243.bin ${UBOOT_OUT}
+	cd $(UBOOT_OUT) && ./tools/mkimage -n rk3328 -T rksd -d ../rk33-firmware/rk3328_ddr_786MHz_v1.06.bin idbloader.img
+	cd $(UBOOT_OUT) && cat ../rk33-firmware/rk3328_miniloader_v2.43.bin >> idbloader.img
+	cd $(UBOOT_OUT) && cp ../rk33-firmware/rk3328_loader_ddr786_v1.06.243.bin ${UBOOT_OUT}
 	${TOOLPATH}/trust_merger trust.ini
 	mv trust.img ${UBOOT_OUT}/
 
